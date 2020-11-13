5a2358ce89f18e14cef19815fc9e8c01db4d190c
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-11-13 10:30:36 +0100

    add linting

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,8 @@ import { makeRenderer } from 'callbag-jsx';
 
 const renderer = makeRenderer();
 renderer.render(<div>Hellow World!</div>).on(document.body);
+
+const x = [1, 2];
+if (x.length === 2) {
+  console.log(x);
+}
