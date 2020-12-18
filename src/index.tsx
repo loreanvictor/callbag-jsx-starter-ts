f1e30da5a8a467e833d86a9e34633dd34ad7c6ee
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-12-18 13:51:18 +0100

    better starting exmaple

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import { makeRenderer } from 'callbag-jsx';
 import { state } from 'callbag-state';
-import { expr, interval } from 'callbag-common';
+import { expr, filter, interval, map, pipe } from 'callbag-common';
 
 
 const renderer = makeRenderer();
 const name = state('World');
 const timer = interval(1000);
 
+const format = (n: string) => n[0].toUpperCase() + n.substr(1);
+const displayName = pipe(
+  name,
+  filter(n => n.length > 0),
+  map(format)
+);
+
 renderer.render(
   <>
-    <div>Hellow {name}!</div>
+    <div>Hellow {displayName}!</div>
     <input type='text' _state={name} placeholder='name ...'/>
 
     <br/><br/>
