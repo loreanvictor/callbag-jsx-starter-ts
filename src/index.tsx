f59d06deb02d54b9b5aca88fbe00be0672b11670
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-12-18 13:40:18 +0100

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import { makeRenderer } from 'callbag-jsx';
+import { state } from 'callbag-state';
+import { expr, interval } from 'callbag-common';
+
 
 const renderer = makeRenderer();
-renderer.render(<div>Hellow World!</div>).on(document.body);
+const name = state('World');
+const timer = interval(1000);
 
-const x = [1, 2];
-if (x.length === 2) {
-  console.log(x);
-}
+renderer.render(
+  <>
+    <div>Hellow {name}!</div>
+    <input type='text' _state={name} placeholder='name ...'/>
+
+    <br/><br/>
+
+    <div><small>You have been here {expr($ => $(timer, -1) + 1)} seconds!</small></div>
+  </>
+).on(document.body);
